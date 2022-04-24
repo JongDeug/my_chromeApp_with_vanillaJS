7a264412f1f7e51ed4b4d6053ba8f85cdedb002a
JongDeug(linux) <pinpoint1998@gmail.com>
2022-04-25 02:08:28 +0900

    4.25

--- js/background.js
+++ js/background.js
@@ -1,10 +1,9 @@
 const images = [
-    "01.jpg",
+    "01.png",
 ];
 
 function paintBackgroundImg(){
     const index = Math.floor(Math.random()*images.length);
-    // document.body.innerHTML += `<img src="../img/${images[index]}">`;
     document.body.style.background = `url(../img/${images[index]}) center top`;
     document.body.style.backgroundSize = "cover";
 }
